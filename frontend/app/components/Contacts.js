ad5c2775b44ab32ffe53bdc2417257884a4cb9fe
George499 <53001250+George499@users.noreply.github.com>
2025-01-16 00:26:18 +0300

    123

--- frontend/app/components/Contacts.js
+++ frontend/app/components/Contacts.js
@@ -13,49 +13,69 @@ const Contacts = () => {
       className="bg-fixed flex flex-col relative bg-[url('/images/bkground_1.png')]"
       id="contacts"
     >
-      <div className="flex content-center justify-center items-center flex-col ">
-        <div className="container flex flex-col w-4/5 lg:w-2/3 h-full items-start justify-center pt-[81px] font-semibold ">
+      <div className="flex content-center justify-center items-center flex-col">
+        <div className="container flex flex-col w-4/5 lg:w-2/3 h-full items-start justify-center py-[81px] font-semibold">
           <div className="w-[51px] h-[12px] mb-[21px] bg-[#fffffe]"></div>
           <h2 className="text-[40px] lg:text-[57px] xl:text-[81px] leading-[3.5rem] lg:leading-[4.5rem] font-bold mb-[57px] text-[#fffffe]">
             СВЯЖИТЕСЬ
             <br /> С НАМИ
           </h2>
           <div className="text-white text-[16px] lg:text-[20px] tracking-normal font-semibold mb-[57px]">
-            <p>ДЛЯ РАЗМЕЩЕНИ АНКЕТЫ НА НАШЕМ РЕСУРСЕ – СВЯЖИТЕСЬ С НАМИ</p>
+            <p>ДЛЯ РАЗМЕЩЕНИЯ АНКЕТЫ НА НАШЕМ РЕСУРСЕ – СВЯЖИТЕСЬ С НАМИ</p>
           </div>
-          <div className="container flex flex-col h-full justify-center ">
-            {isContactsVisible ? (
-              <div className="text-black text-[20px] tracking-normal mb-[81px]">
-                <p>Пишите: want@pluginagency.ru</p>
-                <p className="mb-[57px] hidden lg:block">
-                  Звоните: +7 (965) 246 9191{" "}
-                </p>
-                <div className="mb-4 flex">
-                  <p className="lg:hidden ">Звоните:</p>
-                  <a className="block lg:hidden" href="tel:+7 (965) 246 9191">
-                    +7 (965) 246 9191
-                  </a>
-                </div>
 
-                <div className="flex flex-raw items-center">
-                  <a href="https://wa.me/+79652469191">
-                    <RiWhatsappFill
-                      className={` w-[35px] h-[35px] cursor-pointer text-[#fffffe]`}
-                    />
-                  </a>
-                  <a href="https://t.me/+79652469191">
-                    <FaTelegram className="w-[30px] h-[30px] cursor-pointer ml-3 text-[#fffffe]" />
-                  </a>
-                </div>
-              </div>
-            ) : (
+          {/* Блоки в строку */}
+          <div className="flex flex-col lg:flex-row w-full justify-between items-start lg:items-center gap-6">
+            {/* Блок 1: Размещение анкет */}
+            <div className="flex flex-col items-start">
+              <h3 className="text-[20px] lg:text-[24px] font-bold text-[#fffffe] mb-3">
+                РАЗМЕЩЕНИЯ АНКЕТЫ
+              </h3>
               <button
-                onClick={handleClick}
-                className="lg:w-[293px] lg:h-[61px] text-white text-[20px] font-semibold uppercase duration-300 p-4 text-center mb-[57px] bg-[#3742a3] hover:bg-[#1d2357] text-[#fffffe]"
+                onClick={() => (window.location.href = "/pricing")} // Переход на страницу со стоимостями
+                className="lg:w-[200px] lg:h-[50px] text-white text-[18px] font-semibold uppercase duration-300 p-3 text-center bg-[#3742a3] hover:bg-[#1d2357] text-[#fffffe]"
               >
-                Показать контакты
+                Подробнее
               </button>
-            )}
+            </div>
+
+            {/* Блок 2: Вопросы и предложения */}
+            <div className="flex flex-col items-start">
+              <h3 className="text-[20px] lg:text-[24px] font-bold text-[#fffffe] mb-3">
+                ВОПРОСЫ И ПРЕДЛОЖЕНИЯ
+              </h3>
+              {isContactsVisible ? (
+                <div className="text-black text-[20px] tracking-normal">
+                  <p>Пишите: want@pluginagency.ru</p>
+                  <p className="mb-[57px] hidden lg:block">
+                    Звоните: +7 (965) 246 9191
+                  </p>
+                  <div className="mb-4 flex">
+                    <p className="lg:hidden">Звоните:</p>
+                    <a className="block lg:hidden" href="tel:+7 (965) 246 9191">
+                      +7 (965) 246 9191
+                    </a>
+                  </div>
+                  <div className="flex flex-raw items-center">
+                    <a href="https://wa.me/+79652469191">
+                      <RiWhatsappFill
+                        className={`w-[35px] h-[35px] cursor-pointer text-[#fffffe]`}
+                      />
+                    </a>
+                    <a href="https://t.me/+79652469191">
+                      <FaTelegram className="w-[30px] h-[30px] cursor-pointer ml-3 text-[#fffffe]" />
+                    </a>
+                  </div>
+                </div>
+              ) : (
+                <button
+                  onClick={handleClick}
+                  className="lg:w-[200px] lg:h-[50px] text-white text-[18px] font-semibold uppercase duration-300 p-3 text-center bg-[#3742a3] hover:bg-[#1d2357] text-[#fffffe]"
+                >
+                  Связаться
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
--- frontend/app/components/PopularSpeakers.js
+++ frontend/app/components/PopularSpeakers.js
@@ -1,21 +1,22 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Product from "./Product";
 import { motion } from "framer-motion";
+import Image from "next/legacy/image";
 
-function SixthScreen() {
+const API_BASE_URL = "https://admin.pluginexpert.ru";
+
+function PopularSpeakers() {
   const [popularSpeakers, setPopularSpeakers] = useState([]);
 
   useEffect(() => {
     const fetchPopularSpeakers = async () => {
       try {
         const res = await fetch(
-          "https://admin.pluginexpert.ru/api/popular-speakers"
+          `${API_BASE_URL}/api/popular-speakers?populate=image`
         );
         if (!res.ok) throw new Error("Ошибка загрузки популярных спикеров");
         const data = await res.json();
-        console.log(data);
         setPopularSpeakers(data.data);
       } catch (error) {
         console.error("Ошибка:", error);
@@ -26,32 +27,41 @@ function SixthScreen() {
   }, []);
 
   return (
-    <div className="bg-fixed h-full flex flex-col">
-      <div
-        className={`flex content-center justify-center items-center flex-col bg-white`}
+    <div className="bg-[#42484d]">
+      <motion.div
+        initial={{ x: "-100vw" }}
+        animate={{ x: 0 }}
+        transition={{ type: "spring", stiffness: 50 }}
+        className="grid grid-cols-2 lg:grid-cols-3 gap-0"
       >
-        <div className="w-full h-full items-center justify-center">
-          <div className="flex flex-col justify-center">
-            <div className="bg-[#42484d] mb-[81px] w-full">
-              <div className="grid grid-cols-2 lg:grid-cols-3 gap-0 ">
-                <motion.div
-                  initial={{ x: "-100vw" }}
-                  animate={{ x: 0 }}
-                  transition={{ type: "spring", stiffness: 50 }}
-                >
-                  <div className="grid grid-cols-2 lg:grid-cols-3 gap-0">
-                    {popularSpeakers.map((speaker) => (
-                      <Product key={speaker.id} speaker={speaker} />
-                    ))}
-                  </div>
-                </motion.div>
+        {popularSpeakers?.map((speaker) => (
+          <div className="group relative" key={speaker.id}>
+            <Image
+              src={`${API_BASE_URL}${speaker.image[0].url}`}
+              alt={`Image of ${speaker.name}`}
+              layout="responsive"
+              width="100%"
+              height="100%"
+              objectFit="cover"
+            />
+
+            <div className="hidden absolute w-full h-full top-0 left-0 lg:flex">
+              {/* Прозрачный фон */}
+              <div className="absolute w-full h-full bg-[#4e5ac3] lg:opacity-0 lg:group-hover:opacity-50 opacity-60 transition-opacity-60 duration-700"></div>
+
+              {/* Контент (name и description) */}
+              <div className="cursor-pointer relative z-10 w-full h-full flex flex-col items-center justify-center text-center lg:opacity-0 lg:group-hover:opacity-100 transition-opacity-60 duration-700">
+                <h3 className="text-[30px] text-[#000000]">{speaker.name}</h3>
+                <p className="hidden lg:block lg:text-[15px] text-[#fffffe]">
+                  {speaker.description}
+                </p>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        ))}
+      </motion.div>
     </div>
   );
 }
 
-export default SixthScreen;
+export default PopularSpeakers;
