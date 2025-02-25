8f8a3537c76d7d37925d7a318b30b7920653a51c
George499 <gi.32@mail.ru>
2025-02-25 20:44:05 +0300

    footer

--- frontend/app/components/main-page/Footer.js
+++ frontend/app/components/main-page/Footer.js
@@ -15,8 +15,8 @@ function Footer() {
     "w-[34px] h-[34px] mr-[7px] p-1 cursor-pointer hover:text-slate-300";
 
   return (
-    <footer className="h-[100px] lg:h-[145px] bg-[#2f2e2e] flex content-center justify-center items-center flex-col w-full">
-      <div className="container flex flex-row w-4/5 lg:w-2/3 h-full items-center justify-between font-play text-[16px] lg:text-[20px] font-normal text-white">
+    <footer className="min-h-[100px] lg:min-h-[145px] bg-[#2f2e2e] flex content-center justify-center items-center flex-col w-full pt-4">
+      <div className="container flex flex-row w-4/5 lg:w-2/3 h-full  justify-between font-play text-[16px] lg:text-[20px] font-normal text-white items-start">
         <div>
           <p className="flex flex-col">
             <span className="flex">
@@ -32,10 +32,20 @@ function Footer() {
           <li className="mr-5">
             <Link
               href="/speakers/forSpeekers"
-              className="cursor-pointer hover:text-slate-300"
+              className="cursor-pointer hover:text-slate-300 "
             >
               СПИКЕРАМ – РАЗМЕСТИТЬ АНКЕТУ
             </Link>
+            <div className="text-justify flex flex-col text-[14px] mt-6">
+     <p>ИП ГУЗАНОВСКИЙ АЛЕКСЕЙ СТАНИСЛАВОВИЧ </p> 
+	<p>Расчётный счёт: 40802810940000416321</p>
+	<p>ИНН: 772705597288</p>
+	<p>ОГРНИП: 323774600074372</p>
+	<p>Банк: ПАО Сбербанк</p>
+	<p>БИК: 044525225</p>
+	<p>Кор. счёт: 30101810400000000225</p>
+	
+  </div>
           </li>
           <li>
             <Link
@@ -54,6 +64,8 @@ function Footer() {
           </nav>
         </div> */}
       </div>
+      
+       
     </footer>
   );
 }
